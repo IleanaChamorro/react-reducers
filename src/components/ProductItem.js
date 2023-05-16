7c0283cf459ed116f9f02e6d12bb416d7c798dbd
Ileana <ic.chamorro07@gmail.com>
2023-05-16 14:03:09 -0300

    Carrito de Compras con Reducers (2/5)

---
+++ src/components/ProductItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductItem = ({data}) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ProductItem
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { shoppingInitalState, shoppingReducer } from '../reducers/shoppingReducer'
 
 const ShoppingCart = () => {
+  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitalState);
+  const { products, cart } = state;
   return (
     <div>
         <h2>Carrito de compras</h2>
         <h3>Productos</h3>
         <article className="box">
-            
+
         </article>
         <h3>Carrito</h3>
         <article className="box"></article>
